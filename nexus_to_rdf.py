aeaae50ef71763f54644c5be716e15a6fe487b01
Andrew Edmondson <ed@rameus.org.uk>
2015-10-23 17:20:36 +0100

    auto-commit from ed@roo

--- nexus_to_rdf.py
+++ nexus_to_rdf.py
@@ -3,73 +3,118 @@
 # NOTE: Network will expect amino-acid files to have a .ami extension
 #       and DNA files to have an .rdf extension
 
-# XXX: Still doesn't quite work - compare output with that of DnaSP...
-
-import sys
 from collections import OrderedDict
+import json
+import os
 
-with open(sys.argv[1]) as f:
-    data = f.readlines()
 
-matrix = OrderedDict()
+def convert(inputfile, ignore_missing):
+    """
+    Convert NEXUS to RDF and create a JSON mapping for use by relable_fdi.py.
+    """
+    with open(inputfile) as f:
+        data = f.readlines()
 
-in_matrix = False
-for line in data:
-    line = line.strip()
-    if line.lower() == 'matrix':
-        in_matrix = True
-        continue
+    name = os.path.splitext(inputfile)[0]
 
-    if not in_matrix:
-        continue
+    matrix = OrderedDict()
 
-    if ' ' not in line:
-        continue
+    in_matrix = False
+    is_dna = False
+    is_protien = False
+    for line in data:
+        if "datatype=dna" in line:
+            is_dna = True
+        elif "datatype=protien" in line:
+            is_protien = True
 
-    label, stripe = line.split()
-    matrix[label] = stripe
-
-# total input characters
-n_chars = len(stripe)
-
-GAP = '-'
-MISSING = '?'
-
-print("NOTE: All characters with missing/gap will be ignored")
-ignore_chars = set([])
-for lab in matrix:
-    for i, char in enumerate(matrix[lab]):
-        if char == GAP or char == MISSING:
-            ignore_chars.add(i + 1)
-print("Ignoring characters: {}".format(ignore_chars))
-
-n_included_chars = n_chars - len(ignore_chars)
-
-output = []
-n_header_lines = len(str(n_chars))
-for l in range(n_header_lines):
-    line = ' ' * 7
-    for x in range(n_chars):
-        y = x + 1  # start at 1
-        if y in ignore_chars:
+        line = line.strip()
+        if line.lower() == 'matrix':
+            in_matrix = True
             continue
-        y_s = str(y).ljust(n_header_lines)
-        line += y_s[l]
 
-    output.append(line)
+        if not in_matrix:
+            continue
 
-output.extend([''] * 4)
+        if ' ' not in line:
+            continue
 
-for n, lab in enumerate(matrix):
-    stripe = matrix[lab]
-    output.append("H_{}{}  1".format(str(n + 1).ljust(5), ''.join(x for i, x in enumerate(stripe) if i + 1 not in ignore_chars)))
-    print("\t H_{} is {}".format(n + 1, lab))
+        label, stripe = line.split()
+        matrix[label] = stripe
 
-output.append('')
-output.append('1' * n_included_chars)
-output.append('')
+    if not is_dna and not is_protien:
+        raise ValueError("Not DNA or protien file - help!")
 
-with open(sys.argv[2], 'w') as out:
-    out.write('\n'.join(output))
+    # total input characters
+    n_chars = len(stripe)
 
-print("See {}".format(sys.argv[2]))
+    GAP = '-'
+    MISSING = '?'
+
+    print("NOTE: All characters with missing/gap will be ignored")
+    missing_chars = set([])
+    for lab in matrix:
+        for i, char in enumerate(matrix[lab]):
+            if char == GAP or char == MISSING:
+                missing_chars.add(i + 1)
+    print("Characters with GAP or MISSING: {}".format(missing_chars))
+
+    n_included_chars = n_chars - (len(missing_chars) if ignore_missing else 0)
+
+    output = []
+    n_header_lines = len(str(n_chars))
+    for l in range(n_header_lines):
+        line = ' ' * 7
+        for x in range(n_chars):
+            y = x + 1  # start at 1
+            if y in missing_chars and ignore_missing:
+                continue
+            y_s = str(y).ljust(n_header_lines)
+            line += y_s[l]
+
+        output.append(line)
+
+    output.extend([''] * 4)
+
+    mapping = {}
+    for n, lab in enumerate(matrix):
+        stripe = matrix[lab]
+        if ignore_missing:
+            stripe_s = ''.join(x for i, x in enumerate(stripe) if i + 1 not in ignore_chars)
+        else:
+            stripe_s = ''.join(stripe)
+            stripe_s = stripe_s.replace(GAP, 'N')
+            stripe_s = stripe_s.replace(MISSING, 'N')
+        output.append("H_{}{}  1".format(str(n + 1).ljust(5), stripe_s))
+        print("\t H_{} is {}".format(n + 1, lab))
+        mapping['H_{}'.format(n + 1)] = lab
+
+    output.append('')
+    output.append('1' * n_included_chars)
+    output.append('')
+
+    if is_dna:
+        ext = 'rdf'
+    else:
+        ext = 'ami'
+    outputfile = "{}.{}".format(name, ext)
+    with open(outputfile, 'w') as out:
+        out.write('\n'.join(output))
+
+    mappingfile = "{}.json".format(name)
+    with open(mappingfile, 'w') as m:
+        json.dump(mapping, m)
+
+    print("See {}".format(outputfile))
+    print("Use {} with relable_fdi.py".format(mappingfile))
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Convert a NEXUS file to RDF.")
+    parser.add_argument('nexusfile', help='NEXUS filename')
+    parser.add_argument('--ignore-missing', help='Ignore gaps and missing chars',
+                        action='store_true', default=False)
+    args = parser.parse_args()
+
+    convert(args.nexusfile, args.ignore_missing)
