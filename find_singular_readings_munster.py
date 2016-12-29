d4bf5bed43c2a7d343bca0d21bb12da64eed1808
Andrew Edmondson <ed@rameus.org.uk>
2016-12-29 15:26:16 +0000

    auto-commit from ed@roo

--- find_singular_readings_munster.py
+++ find_singular_readings_munster.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python3
 """
-Look through all manuscripts to find the closest and furthest pairs...
+Look through all manuscripts looking for singular readings
 """
 import MySQLdb
 import itertools
-from compare_mss_munster import compare
 
 def compare_all(host, db, user, password, table):
     """
     Connect to the mysql db and loop through what we find
     """
-    print "\nComparison of all witnesses in db {}:{}".format(db, table)
+    print("\nComparison of all witnesses in db {}:{}".format(db, table))
 
     db = MySQLdb.connect(host=host, user=user, passwd=password, db=db, charset='utf8')
     cur = db.cursor()
@@ -28,20 +28,32 @@ def compare_all(host, db, user, password, table):
     #~ cur.execute("OPTIMIZE TABLE {}_ed_map".format(table))
     #~ print "Done"
 
-    query = """SELECT COUNT(A.vu_id) FROM Ch18Att_ed_map A INNER JOIN Ch18Att_ed_map B ON A.vu_id=B.vu_id AND A.witness=01 AND B.witness=05 AND A.ident != B.ident"""
-    cur.execute(query)
-    for x in cur.fetchall():
-        print x
+    #~ query = """SELECT COUNT(A.vu_id) FROM Ch18Att_ed_map A INNER JOIN Ch18Att_ed_map B ON A.vu_id=B.vu_id AND A.witness=01 AND B.witness=05 AND A.ident != B.ident"""
+    #~ cur.execute(query)
+    #~ for x in cur.fetchall():
+        #~ print x
 
-    return
+    #~ return
 
-    query = """SELECT DISTINCT witness FROM {}_ed_map ORDER BY witness""".format(table)
-    cur.execute(query)
-    witnesses = list(cur.fetchall())
+    #~ query = """SELECT DISTINCT witness FROM {}_ed_map ORDER BY witness""".format(table)
+    #~ cur.execute(query)
+    #~ witnesses = list(cur.fetchall())
 
-    pairs = list(itertools.combinations(witnesses, 2))
+    #~ pairs = list(itertools.combinations(witnesses, 2))
 
-    print len(pairs)
+    #~ print len(pairs)
+
+    cur.execute("SELECT vu_id FROM {}_ed_map".format(table))
+    vu_ids = sorted(set(x[0] for x in cur.fetchall()))
+    for vu in vu_ids:
+        # NOTE: the GROUP_CONCAT things only make sense for count == 1...
+        query = "SELECT GROUP_CONCAT(witness), vu_id, ident, GROUP_CONCAT(greek), COUNT(ident) as count FROM {}_ed_map WHERE vu_id = {} GROUP BY ident".format(table, vu)
+        cur.execute(query)
+        for (wits, vu_id, ident, greek, count) in cur.fetchall():
+            if count != 1:
+                continue
+            print("Found singular reading: Witness {} reads {} in VU {}.".format(wits, greek, vu_id))
+
 
 if __name__ == "__main__":
     import argparse
@@ -59,4 +71,3 @@ if __name__ == "__main__":
                 args.mysql_user,
                 args.mysql_password,
                 args.mysql_table)
-
